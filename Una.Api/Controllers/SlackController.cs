7f4bf560fb757637cae1575d1a4a614f3f93d444
Viktor Kudria <viktor.kudria@creuna.com>
2018-05-21 10:43:10 +0300

    Add endpoint for slack verification

---
+++ Una.Api/Controllers/SlackController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Una.Api.Models.Slack;
+
+namespace Una.Api.Controllers
+{
+    [Route("api/slack")]
+    public class SlackController : Controller
+    {
+        [HttpPost]
+        public string Post([FromBody]SlackVerificationMessage value)
+        {
+            return value.Challenge;
+        }
+    }
+}
---
+++ Una.Api/Models/Slack/SlackVerificationMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Una.Api.Models.Slack
+{
+    public class SlackVerificationMessage
+    {
+        public string Token { get; set; }
+
+        public string Challenge { get; set; }
+
+        public string Type { get; set; }
+    }
+}
