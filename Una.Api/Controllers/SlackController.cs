496850d55635ee210f41428e66d772ccf0898c38
Viktor Kudria <viktor.kudria@creuna.com>
2018-05-22 15:37:36 +0300

    Add simple response logic

--- Una.Api/Controllers/SlackController.cs
+++ Una.Api/Controllers/SlackController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Slack.Webhooks;
 using Una.Api.Models.Slack;
 
 namespace Una.Api.Controllers
@@ -9,7 +10,27 @@ namespace Una.Api.Controllers
         [HttpPost]
         public string Post([FromBody]SlackEventWrapper value)
         {
-            return value.Challenge;
+            if (!string.IsNullOrEmpty(value.Challenge))
+            {
+                return value.Challenge;
+            }
+
+            if (value.Event.Type.Equals("message") && !string.IsNullOrEmpty(value.Event.User))
+            {
+                var slackClient = new SlackClient("https://hooks.slack.com/services/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");
+
+                var slackMessage = new SlackMessage
+                {
+                    Channel = value.Event.Channel,
+                    Text = $"I\'m sorry, I don\'t understand! Sometimes I have an easier time with a few simple keywords.",
+                    IconEmoji = Emoji.Pizza,
+                    Username = "Una Bot"
+                };
+
+                slackClient.Post(slackMessage);
+            }
+
+            return string.Empty;
         }
     }
 }
